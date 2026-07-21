3ebb041b9f3f9525a6a182f2a42c4c6e93cc9066
amacias <amaciascaceres@gmail.com>
2026-07-21 18:27:56 +0200

    Day 24: Deploy task-service to minikube with liveness and readiness probes
       Add infra/k8s/task-service/ with Deployment and Service manifests.
       Postgres and Redis run via docker-compose on the host and are reached
       from the cluster through host.minikube.internal.

--- infra/docker/init.sql
+++ infra/docker/init.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE taskflow;
-CREATE DATABASE taskflow_users;
+SELECT 'CREATE DATABASE taskflow' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'taskflow')\gexec
+SELECT 'CREATE DATABASE taskflow_users' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'taskflow_users')\gexec
 GRANT ALL PRIVILEGES ON DATABASE taskflow TO taskflow;
 GRANT ALL PRIVILEGES ON DATABASE taskflow_users TO taskflow;
